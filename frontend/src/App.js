f35e4af962a2953f015021119973931ead9dd0f3
Miguel Rosa <miguelrosa.programacao@gmail.com>
2024-08-15 17:00:31 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Banner from './components/Banner';
 import Menubar from './components/Menubar';
 import TeamTable from './components/TeamTable';
-import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
             <Menubar />
             <Banner />
             <TeamTable />
-            <Footer />
         </div>
     );
 }
